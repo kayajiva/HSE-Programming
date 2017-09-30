666de621ca4438f2ba15ae6bbe5c7fb7c2abbfaa
kayajiva <kayajiva@gmail.com>
2017-09-30 16:35:24 +0300

    first commit

---
+++ hw1/30.09.17.py
@@ -0,0 +1,12 @@
+a = int(input("Введите число:"))
+b = int(input("И ещё одно:"))
+c = int(input("И третье:"))
+if (a % b == c): 
+    print("1_True")
+else:
+    print("1_False")
+    
+if (a/b == c):
+    print("2_True")
+else:
+    print("2_False")
