abfd1c322eeab80983004ac5ca53e15ec27117c1
Maria Chudnovskaya <31654702+kayajiva@users.noreply.github.com>
2017-11-21 20:32:47 +0300

    Add files via upload

---
+++ hw4/6 вариант. 21.11.17.py
@@ -0,0 +1,13 @@
+fname = r"C:\Users\User\text.txt"
+with open(fname, encoding="utf-8") as f:
+    text=f.read()
+words = text.split()
+cap = 0
+low = 0
+for word in words:
+    if word.istitle():
+        cap+=1
+    else:
+        low+=1
+percent = cap/(cap+low)*100
+print ("Процент слов с заглавной буквы:", int(percent),"%")
