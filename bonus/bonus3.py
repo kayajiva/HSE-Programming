a0746825f330807b102d85406ddb6bf7424b9178
Maria Chudnovskaya <31654702+kayajiva@users.noreply.github.com>
2017-12-07 19:41:18 +0300

    Add files via upload

---
+++ bonus/bonus3.py
@@ -0,0 +1,14 @@
+text = input("Print your text here: ")
+words = text.split()
+vowels = ["e", "y", "u", "i", "o", "a", "E", "Y", "U", "I", "O", "A"]
+ntext=[]
+for word in words:
+    for letter in word[0]:
+        if not letter in vowels:
+            nword = word[1:] + word[0].lower() + "ay"
+            ntext.append(nword)
+        else:
+            nword = word[0].lower() + word[1:] + "ay"
+            ntext.append(nword)
+fin = " ".join(ntext)
+print(fin)
